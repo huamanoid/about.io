9a236bb3388723cca80107bc18915575cf165d24
Aman Kumar <aman18a@iitg.ac.in>
2020-12-13 22:04:46 +0530

    v1.6
    wanted noise added

--- assets/js/main.js
+++ assets/js/main.js
@@ -339,6 +339,50 @@ box4.onclick = () => {
   }, 300);
 };
 
+// technique for this demo found here 
+// http://stackoverflow.com/questions/22003491/animating-canvas-to-look-like-tv-noise
+
+const canvas = document.querySelector('canvas'),
+				 ctx = canvas.getContext('2d')
+
+canvas.width = canvas.height = 128
+
+resize();
+window.onresize = resize;
+
+function resize() {
+	canvas.width = window.innerWidth * window.devicePixelRatio / 1
+	canvas.height = window.innerHeight * window.devicePixelRatio / 1
+	canvas.style.width = window.innerWidth + 'px'
+	canvas.style.height = window.innerHeight + 'px'
+}
+
+
+
+// window.setInterval('noise(ctx)',150);
+function noise(ctx) {
+    
+	const w = ctx.canvas.width,
+				h = ctx.canvas.height,
+				iData = ctx.createImageData(w, h),
+				buffer32 = new Uint32Array(iData.data.buffer),
+				len = buffer32.length
+	  let i = 1
+
+	for(; i < len;i++)
+		
+		if (Math.random() < 0.5) buffer32[i] = 0xffffffff;
+
+		ctx.putImageData(iData, 0, 0);
+}
+
+
+
+(function loop() {
+    noise(ctx);
+    requestAnimationFrame(loop);
+})();
+
 retype.start();
 
 
